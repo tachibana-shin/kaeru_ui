9f514a0c78b09af83b43881d1ed226ef7126e805
Tachibana Shin <tachibshin@duck.com>
2025-09-24 01:46:56 +0700

    Add optional vertical parameter to margin and padding methods
    Enhance the `m` and `p` methods to accept an optional vertical value,
    allowing for more flexible margin and padding adjustments.

--- lib/extensions/margin.dart
+++ lib/extensions/margin.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:kaeru_ui/extensions/box.dart';
 
 extension KaeruMarginExtension on Widget {
-  Widget m(double v) => box.m(v).make();
+  Widget m(double v, [double? y]) =>
+      y == null ? box.m(v).make() : box.mx(v).my(y).make();
   Widget mx(double v) => box.mx(v).make();
   Widget my(double v) => box.my(v).make();
   Widget mt(double v) => box.mt(v).make();
--- lib/extensions/padding.dart
+++ lib/extensions/padding.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:kaeru_ui/extensions/generic/edge_insets.dart';
 import 'package:kaeru_ui/extensions/generic/num.dart';
 
 extension KaeruPaddingExtension on Widget {
   Widget pad(EdgeInsets insets) => Padding(padding: insets, child: this);
-  Widget p(double v) => Padding(padding: v.p, child: this);
+  Widget p(double v, [double? y]) =>
+      Padding(padding: y == null ? v.p : v.px.merge(y.py), child: this);
   Widget px(double v) => Padding(padding: v.px, child: this);
   Widget py(double v) => Padding(padding: v.py, child: this);
   Widget pt(double v) => Padding(padding: v.pt, child: this);
