b6b2b0061fc68c7f7ee027bbdf87b3d570a3ab58
Tachibana Shin <tachibshin@duck.com>
2025-09-24 02:38:28 +0700

    Update spacing.dart

--- lib/extensions/spacing.dart
+++ lib/extensions/spacing.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 extension KaeruSpacing on num {
   // Horizontal spacing: SizedBox(width: ...)
   Widget get hSpace => SizedBox(width: toDouble());
+  Widget get widthBox => hSpace;
 
   // Vertical spacing: SizedBox(height: ...)
   Widget get vSpace => SizedBox(height: toDouble());
+  Widget get heightBox => vSpace;
 }
