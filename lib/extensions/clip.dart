4942e76b20174dbb6beda16db0b25fcd7a82cfcb
Tachibana Shin <tachibshin@duck.com>
2025-09-24 12:24:12 +0700

    Add new clipping methods to KaeruWidgetClipExtension
    Introduce methods to set width, height, and size for widgets.

--- lib/extensions/clip.dart
+++ lib/extensions/clip.dart
@@ -8,4 +8,9 @@ extension KaeruWidgetClipExtension on Widget {
 
   Widget square(double size) =>
       SizedBox(width: size, height: size, child: this);
+
+  Widget width(double width) => SizedBox(width: width, child: this);
+  Widget height(double height) => SizedBox(height: height, child: this);
+  Widget size(double width, double height) =>
+      SizedBox(width: width, height: height, child: this);
 }
